eb74b303bf50506d847cc76ae1d00aa15365a6e7
toriumi <r202203428zu@jindai.jp>
2023-12-21 16:09:54 +0900

    変数に入れてprint helloしたのち、文字列全体とその長さを表示

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include <string.h>
 
 int main(void){
     printf("hello\n");
 
     char str[] = "hello";
     printf("str[]の文字は:%s\n",str);
+    printf("文字列の長さ: %zu\n", strlen(str));
     return 0;
 }
